a2a54834a985a7c9b5d44d5a5510ef305bec6f54
Youssef Assem <yo_90_32@hotmail.com>
2019-12-31 17:31:23 +0200

    Lesson 3
    z-buffer addition

--- J3ossGL/main.cpp
+++ J3ossGL/main.cpp
@@ -8,37 +8,6 @@ const int height = 800;
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
-void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
-	bool steep = false;
-	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-		std::swap(x0, y0);
-		std::swap(x1, y1);
-		steep = true;
-	}
-	if (x0 > x1) {
-		std::swap(x0, x1);
-		std::swap(y0, y1);
-	}
-	int dx = x1 - x0;
-	int dy = y1 - y0;
-	int derror2 = std::abs(dy) * 2;
-	int error2 = 0;
-	int y = y0;
-	for (int x = x0; x <= x1; x++) {
-		if (steep) {
-			image.set(y, x, color);
-		}
-		else {
-			image.set(x, y, color);
-		}
-		error2 += derror2;
-		if (error2 > dx) {
-			y += (y1 > y0 ? 1 : -1);
-			error2 -= dx * 2;
-		}
-	}
-}
-
 void cpu_optimized_triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage& image, TGAColor color){
 	if (v0.y == v1.y && v0.y == v2.y) return;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 	if (v0.y > v1.y) std::swap(v0, v1);
@@ -119,7 +88,7 @@ void cpu_optimized_triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage& image, TGACo
 	}
 }
 
-Vec3f barycentric(Vec2i *pts,Vec2i p)
+Vec3f barycentric(Vec3f *pts,Vec3f p)
 {
 	Vec3f vx = Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - p[0]);
 	Vec3f vy = Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - p[1]);
@@ -129,31 +98,37 @@ Vec3f barycentric(Vec2i *pts,Vec2i p)
 	if (std::abs(bc[2]) < 1) return Vec3f(-1, 1, 1);
 	return Vec3f(1.f - (bc.x + bc.y) / bc.z, bc.y / bc.z, bc.x / bc.z);
 }
-
-void triangle(Vec2i *pts,TGAImage &image,float intensity)
+void triangle(Vec3f *pts,float *z_buffer,TGAImage &image,float intensity)
 {
-	Vec2i bbox_min = Vec2i(image.get_width() - 1, image.get_height() - 1);
-	Vec2i bbox_max = Vec2i(0, 0);
-	Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+	Vec2f bbox_min = Vec2f(image.get_width() - 1.f, image.get_height() - 1.f);
+	Vec2f bbox_max = Vec2f(0.f, 0.f);
+	Vec2f clamp(image.get_width() - 1.f, image.get_height() - 1.f);
 
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 2; j++)
 		{
-			bbox_min[j] = std::max(0, std::min(bbox_min[j], pts[i][j]));
+			bbox_min[j] = std::max(0.f, std::min(bbox_min[j], pts[i][j]));
 			bbox_max[j] = std::min(clamp[j], std::max(bbox_max[j], pts[i][j]));
 		}
 	}
 
-	Vec2i p;
-	for (p.x = bbox_min.x; p.x < bbox_max.x; p.x++)
+	Vec3f p;
+	for (p.x = bbox_min.x; p.x <= bbox_max.x; p.x++)
 	{
-		for (p.y = bbox_min.y; p.y < bbox_max.y; p.y++)
+		for (p.y = bbox_min.y; p.y <= bbox_max.y; p.y++)
 		{
 			Vec3f bc = barycentric(pts, p);
 
 			if (bc.x < 0 || bc.y < 0 || bc.z < 0) continue;
-			image.set(p.x, p.y, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+
+			p.z = bc * Vec3f(pts[0][2], pts[1][2], pts[2][2]);
+
+			if (z_buffer[ (int)(p.x + p.y* width) ] < p.z)
+			{
+				z_buffer[ (int)(p.x + p.y * width) ] = p.z;
+				image.set((int)p.x, (int)p.y, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+			}		
 		}
 	}
 }
@@ -165,17 +140,23 @@ int main(int argc, char** argv)
 
 	Vec3f light_dir(0, 0, -1);
 
+	float* z_buffer = new float[width * height];
+	for (int i = 0; i < width*height; i++)
+	{
+		z_buffer[i] = -std::numeric_limits<float>::max();
+	}
+
 	for (int i = 0; i < m.nfaces(); i++)
 	{
 		std::vector<int> face = m.face(i);
 
 		Vec3f world_co[3];
-		Vec2i screen_co[3];
+		Vec3f screen_co[3];
 		
 		for (int j = 0; j < 3; j++)
 		{
 			world_co[j] = m.vert(face[j]);
-			screen_co[j] = Vec2i( (m.vert(face[j]).x + 1) * (width / 2) , (m.vert(face[j]).y + 1) * (height / 2) );
+			screen_co[j] = Vec3f( int((world_co[j].x + 1.) * (width / 2)) , int((world_co[j].y + 1.) * (height / 2)), m.vert(face[j]).z);
 		}
 
 		Vec3f normal = cross(Vec3f(world_co[2] - world_co[0]) ,Vec3f(world_co[1] - world_co[0]));
@@ -185,7 +166,7 @@ int main(int argc, char** argv)
 
 		if (intensity > 0)
 		{
-			triangle(screen_co, image, intensity);
+			triangle(screen_co, z_buffer, image, intensity);
 		}
 	}
 
