f8a7c433bdc391d32f7fb3848fc9e04cc03cf391
Youssef Assem <yo_90_32@hotmail.com>
2019-12-31 05:51:59 +0200

    Lesson 2 - Part 2

--- J3ossGL/main.cpp
+++ J3ossGL/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include "tgaimage.h"
 #include "model.h"
 
@@ -38,8 +39,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
 	}
 }
 
-void cpu_optimized_triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage& image, TGAColor color)
-{
+void cpu_optimized_triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage& image, TGAColor color){
 	if (v0.y == v1.y && v0.y == v2.y) return;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 	if (v0.y > v1.y) std::swap(v0, v1);
 	if (v0.y > v2.y) std::swap(v0, v2);
@@ -68,60 +68,50 @@ void cpu_optimized_triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage& image, TGACo
 	int xcounter12 = v1.x;
 	int dy122 = dy12 * 2;
 
-	for (; y <= v1.y; y++)
-	{
+	for (; y <= v1.y; y++){
 		error01 += derror01;
-		while (error01 > dy01 && dy01 !=0)
-		{
+		while (error01 > dy01 && dy01 !=0){
 			xcounter01 += (v1.x > v0.x ? 1 : -1);
 			error01 -= dy012;
 		}
 
 		error02 += derror02;
-		while (error02 > dy02 && dy02 != 0)
-		{
+		while (error02 > dy02 && dy02 != 0){
 			xcounter02 += (v2.x > v0.x ? 1 : -1);
 			error02 -= dy022;
 		}
 
-		if (xcounter01 > xcounter02)
-		{
+		if (xcounter01 > xcounter02){
 			for (int j = xcounter02; j <= xcounter01; j++) {
 				image.set(j, y, color);
 			}
 		}
-		else
-		{
+		else{
 			for (int j = xcounter01; j <= xcounter02; j++) {
 				image.set(j, y, color);
 			}
 		}
 	}
 
-	for (; y <= v2.y; y++)
-	{
+	for (; y <= v2.y; y++){
 		error12 += derror12;
-		while (error12 > dy12 && dy12 != 0)
-		{
+		while (error12 > dy12 && dy12 != 0){
 			xcounter12 += (v2.x > v1.x ? 1 : -1);
 			error12 -= dy122;
 		}
 
 		error02 += derror02;
-		while (error02 > dy02 && dy02 != 0)
-		{
+		while (error02 > dy02 && dy02 != 0){
 			xcounter02 += (v2.x > v0.x ? 1 : -1);
 			error02 -= dy022;
 		}
 
-		if (xcounter12 > xcounter02)
-		{
+		if (xcounter12 > xcounter02){
 			for (int j = xcounter02; j <= xcounter12; j++) {
 				image.set(j, y, color);
 			}
 		}
-		else
-		{
+		else{
 			for (int j = xcounter12; j <= xcounter02; j++) {
 				image.set(j, y, color);
 			}
@@ -129,10 +119,50 @@ void cpu_optimized_triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage& image, TGACo
 	}
 }
 
-int main(int argc, char** argv) {
-	TGAImage image(width, height, TGAImage::RGB);
+Vec3f barycentric(Vec2i *pts,Vec2i p)
+{
+	Vec3f vx = Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - p[0]);
+	Vec3f vy = Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - p[1]);
 
+	Vec3f bc = cross(vx, vy);
+
+	if (std::abs(bc[2]) < 1) return Vec3f(-1, 1, 1);
+	return Vec3f(1.f - (bc.x + bc.y) / bc.z, bc.y / bc.z, bc.x / bc.z);
+}
+
+void triangle(Vec2i *pts,TGAImage &image,float intensity)
+{
+	Vec2i bbox_min = Vec2i(image.get_width() - 1, image.get_height() - 1);
+	Vec2i bbox_max = Vec2i(0, 0);
+	Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			bbox_min[j] = std::max(0, std::min(bbox_min[j], pts[i][j]));
+			bbox_max[j] = std::min(clamp[j], std::max(bbox_max[j], pts[i][j]));
+		}
+	}
+
+	Vec2i p;
+	for (p.x = bbox_min.x; p.x < bbox_max.x; p.x++)
+	{
+		for (p.y = bbox_min.y; p.y < bbox_max.y; p.y++)
+		{
+			Vec3f bc = barycentric(pts, p);
+
+			if (bc.x < 0 || bc.y < 0 || bc.z < 0) continue;
+			image.set(p.x, p.y, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+		}
+	}
+}
+
+int main(int argc, char** argv) 
+{
+	TGAImage image(width, height, TGAImage::RGB);
 	Model m = Model("obj/african_head.obj");
+
 	Vec3f light_dir(0, 0, -1);
 
 	for (int i = 0; i < m.nfaces(); i++)
@@ -148,14 +178,14 @@ int main(int argc, char** argv) {
 			screen_co[j] = Vec2i( (m.vert(face[j]).x + 1) * (width / 2) , (m.vert(face[j]).y + 1) * (height / 2) );
 		}
 
-		Vec3f normal = ((world_co[2] - world_co[0]) ^ (world_co[1] - world_co[0]));
+		Vec3f normal = cross(Vec3f(world_co[2] - world_co[0]) ,Vec3f(world_co[1] - world_co[0]));
 		normal.normalize();
 
 		float intensity = normal * light_dir;
 
 		if (intensity > 0)
 		{
-			cpu_optimized_triangle(screen_co[0], screen_co[1], screen_co[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+			triangle(screen_co, image, intensity);
 		}
 	}
 
