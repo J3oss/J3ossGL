e5ca9ec7383d5f1dc90e011ae2f26375efe5d049
Youssef Assem <yo_90_32@hotmail.com>
2020-01-02 03:28:40 +0200

    Lesson 5
    adding the camera

--- J3ossGL/main.cpp
+++ J3ossGL/main.cpp
@@ -2,100 +2,61 @@
 #include <cmath>
 #include <limits>
 #include <algorithm>
+
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
 
 const int width = 800;
 const int height = 800;
+const int depth = 255;
 
 Model* m = NULL;
-float* z_buffer = NULL;
+int* z_buffer = NULL;
 
 Vec3f camera(0, 0, 3);
+Vec3f center(0, 0, 0);
 Vec3f light_dir(0, 0, -1);
+Vec3f up(0, 1, 0);
 
-void cpu_optimized_triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage& image, TGAColor color){
-	if (v0.y == v1.y && v0.y == v2.y) return;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-	if (v0.y > v1.y) std::swap(v0, v1);
-	if (v0.y > v2.y) std::swap(v0, v2);
-	if (v1.y > v2.y) std::swap(v1, v2);
+Matrix ViewPort(int x, int y, int w, int h) {
+	Matrix m = Matrix::identity();
+	m[0][3] = x + w / 2.f;
+	m[1][3] = y + h / 2.f;
+	m[2][3] = depth / 2.f;
 
-	int y = v0.y;
-
-	int dx01 = v1.x - v0.x;
-	int dy01 = v1.y - v0.y;
-	int derror01 = std::abs(dx01) * 2;
-	int error01 = 0;
-	int xcounter01 = v0.x;
-	int dy012 = dy01 * 2;
-
-	int dx02 = v2.x - v0.x;
-	int dy02 = v2.y - v0.y;
-	int derror02 = std::abs(dx02) * 2;
-	int error02 = 0;
-	int xcounter02 = v0.x;
-	int dy022 = dy02 * 2;
-
-	int dx12 = v2.x - v1.x;
-	int dy12 = v2.y - v1.y;
-	int derror12 = std::abs(dx12) * 2;
-	int error12 = 0;
-	int xcounter12 = v1.x;
-	int dy122 = dy12 * 2;
-
-	for (; y <= v1.y; y++){
-		error01 += derror01;
-		while (error01 > dy01 && dy01 !=0){
-			xcounter01 += (v1.x > v0.x ? 1 : -1);
-			error01 -= dy012;
-		}
-
-		error02 += derror02;
-		while (error02 > dy02 && dy02 != 0){
-			xcounter02 += (v2.x > v0.x ? 1 : -1);
-			error02 -= dy022;
-		}
-
-		if (xcounter01 > xcounter02){
-			for (int j = xcounter02; j <= xcounter01; j++) {
-				image.set(j, y, color);
-			}
-		}
-		else{
-			for (int j = xcounter01; j <= xcounter02; j++) {
-				image.set(j, y, color);
-			}
-		}
+	m[0][0] = w / 2.f;
+	m[1][1] = h / 2.f;
+	m[2][2] = depth / 2.f;
+	return m;
+}
+Matrix ModelView(Vec3f camera, Vec3f center, Vec3f up) {
+	Vec3f z = (camera - center).normalize();
+	Vec3f x = cross(up, z).normalize();
+	Vec3f y = cross(z, x).normalize();
+	Matrix m_inv = Matrix::identity();
+	Matrix tr = Matrix::identity();
+	for (int i = 0; i < 3; i++) {
+		m_inv[0][i] = x[i];
+		m_inv[1][i] = y[i];
+		m_inv[2][i] = z[i];
+		tr[i][3] = -center[i];
 	}
-
-	for (; y <= v2.y; y++){
-		error12 += derror12;
-		while (error12 > dy12 && dy12 != 0){
-			xcounter12 += (v2.x > v1.x ? 1 : -1);
-			error12 -= dy122;
-		}
-
-		error02 += derror02;
-		while (error02 > dy02 && dy02 != 0){
-			xcounter02 += (v2.x > v0.x ? 1 : -1);
-			error02 -= dy022;
-		}
-
-		if (xcounter12 > xcounter02){
-			for (int j = xcounter02; j <= xcounter12; j++) {
-				image.set(j, y, color);
-			}
-		}
-		else{
-			for (int j = xcounter12; j <= xcounter02; j++) {
-				image.set(j, y, color);
-			}
-		}
-	}
+	return m_inv * tr;
+}
+Matrix V2M(Vec3f v) {
+	Matrix m = Matrix::identity();
+	m[0][0] = v.x;
+	m[1][0] = v.y;
+	m[2][0] = v.z;
+	m[3][0] = 1.f;
+	return m;
+}
+Vec3f M2V(Matrix m) {
+	return Vec3f(m[0][0] / m[3][0], m[1][0] / m[3][0], m[2][0] / m[3][0]);
 }
 
-Vec3f barycentric(Vec3f *pts,Vec3f p)
+Vec3f Barycentric(Vec3i *pts,Vec3f p)
 {
 	Vec3f vx = Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - p[0]);
 	Vec3f vy = Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - p[1]);
@@ -105,27 +66,27 @@ Vec3f barycentric(Vec3f *pts,Vec3f p)
 	if (std::abs(bc[2]) < 1) return Vec3f(-1, 1, 1);
 	return Vec3f(1.f - (bc.x + bc.y) / bc.z, bc.y / bc.z, bc.x / bc.z);
 }
-void triangle(Vec3f *pts,Vec2i *uv_pts,float *z_buffer,TGAImage &image,float intensity)
+void Triangle(Vec3i *pts,Vec2i *uv_pts,int *z_buffer,TGAImage &image,float intensity)
 {
-	Vec2f bbox_min = Vec2f(image.get_width() - 1.f, image.get_height() - 1.f);
-	Vec2f bbox_max = Vec2f(0.f, 0.f);
-	Vec2f clamp(image.get_width() - 1.f, image.get_height() - 1.f);
+	Vec2i bbox_min = Vec2f(image.get_width() - 1, image.get_height() - 1);
+	Vec2i bbox_max = Vec2f(0, 0);
+	Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
 
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 2; j++)
 		{
-			bbox_min[j] = std::max(0.f, std::min(bbox_min[j], pts[i][j]));
+			bbox_min[j] = std::max(0, std::min(bbox_min[j], pts[i][j]));
 			bbox_max[j] = std::min(clamp[j], std::max(bbox_max[j], pts[i][j]));
 		}
 	}
 
-	Vec3f p;
+	Vec3i p;
 	for (p.x = bbox_min.x; p.x <= bbox_max.x; p.x++)
 	{
 		for (p.y = bbox_min.y; p.y <= bbox_max.y; p.y++)
 		{
-			Vec3f bc = barycentric(pts, p);
+			Vec3f bc = Barycentric(pts, p);
 
 			if (bc.x < 0 || bc.y < 0 || bc.z < 0) continue;
 
@@ -146,30 +107,27 @@ void triangle(Vec3f *pts,Vec2i *uv_pts,float *z_buffer,TGAImage &image,float int
 	}
 }
 
-Vec3f m2v(Matrix m) {
-	return Vec3f(m[0][0] / m[3][0], m[1][0] / m[3][0], m[2][0] / m[3][0]);
-}
-Matrix v2m(Vec3f v) {
-	Matrix m = Matrix::identity();
-	m[0][0] = v.x;
-	m[1][0] = v.y;
-	m[2][0] = v.z;
-	m[3][0] = 1.f;
-	return m;
-}
-
 int main(int argc, char** argv) 
 {
 	TGAImage image(width, height, TGAImage::RGB);
 	m = new Model("obj/african_head/african_head.obj");
 
-	Matrix projection = Matrix::identity();
-	projection[3][2] = -1.f / camera.z;
+	Matrix model = Matrix::identity();	//can be used to setup the scene
+	model[2][3] = -5;
 
-	z_buffer = new float[width * height];
+	Matrix model_view = ModelView(camera, center, up);
+
+	Matrix projection = Matrix::identity();
+	projection[3][2] = -1.f / camera.z-center.z;
+
+	Matrix view_port = ViewPort(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+
+	Matrix z = (view_port * projection * model_view * model);
+
+	z_buffer = new int[width * height];
 	for (int i = 0; i < width*height; i++)
 	{
-		z_buffer[i] = -std::numeric_limits<float>::max();
+		z_buffer[i] = std::numeric_limits<int>::min();
 	}
 
 	for (int i = 0; i < m->nfaces(); i++)
@@ -177,15 +135,13 @@ int main(int argc, char** argv)
 		std::vector<int> face = m->face(i);
 
 		Vec3f world_co[3];
-		Vec3f screen_co[3];
+		Vec3i screen_co[3];
 		Vec2i text_co[3];
 
 		for (int j = 0; j < 3; j++)
 		{
 			world_co[j] = m->vert(face[j]);
-
-			screen_co[j] = m2v(projection * v2m(world_co[j]));
-			screen_co[j] = Vec3f( int((screen_co[j].x + 1.) * (width / 2)) , int((screen_co[j].y + 1.) * (height / 2)), m->vert(face[j]).z);
+			screen_co[j] = M2V(z * V2M(world_co[j]));
 		}
 
 		Vec3f normal = cross(Vec3f(world_co[2] - world_co[0]) , Vec3f(world_co[1] - world_co[0]));
@@ -199,7 +155,7 @@ int main(int argc, char** argv)
 			{
 				text_co[j] = m->uv(i, j);
 			}
-			triangle(screen_co,text_co ,z_buffer, image, intensity);
+			Triangle(screen_co,text_co ,z_buffer, image, intensity);
 		}
 	}
 
